2afdd93d058bd723e8e53b93afa1225d39bd5314
Heinzino <heinzveintimilla@hotmail.com>
2025-11-29 15:19:05 -0700

    feat: SEO Critical changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.ambitionfitness.ca',
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
+    }),
+    sitemap({
+      filter: (page) => !page.includes('/clients') && !page.includes('/contact-us') && !page.includes('/private-facility'),
     }),
   ],
 });
